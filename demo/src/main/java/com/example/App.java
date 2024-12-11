062866d6586c1bd10790812b90a4fc45ec4929d9
Zahoris.Leonardo <zahoris.leonardo@one-beyond.com>
2024-12-11 13:46:07 +0100

    changes maded

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,17 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World 1!" );
+                System.out.println( "Hello World 2!" );
+                        System.out.println( "Hello World 3!" );
+
+
+    }
+}
